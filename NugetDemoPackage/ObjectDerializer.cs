3d5c25bbfae0bbce4b0da76bbe78783737b88371
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-04-20 01:06:04 +0100

    added project files

---
+++ NugetDemoPackage/ObjectDerializer.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace NugetDemoPackage {
+    public static class ObjectDerializer {
+        public static T Deserialize<T>(string jsonObjectString) {
+            return JsonConvert.DeserializeObject<T>(jsonObjectString);
+        }
+    }
+}
---
+++ NugetDemoPackage/ObjectSerializer.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace NugetDemoPackage {
+    public static class ObjectSerializer {
+        public static string Serialize<T>(T objectToSerialize) where T: class {
+            return JsonConvert.SerializeObject(objectToSerialize);
+        }
+    }
+}
